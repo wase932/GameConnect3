ae367f5399d362a2441cdff37dc9b14d3cd0c20c
Toluwase <wase932@gmail.com>
2017-10-31 05:23:19 -0700

    Partial implementation of Play algorithm. As suspected, touch event fires twice.

--- app/src/main/java/com/example/toluwase/gameconnect3/MainActivity.java
+++ app/src/main/java/com/example/toluwase/gameconnect3/MainActivity.java
@@ -8,13 +8,23 @@ import android.view.*;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-    private float[] touchCoord = new float[2];
-    private ArrayList<float[]> allViewCoords = new ArrayList<>();
+    private float[] TouchCoord = new float[2];
+    private ArrayList<float[]> AllViewCoords = new ArrayList<>();
+    private boolean IsFirstPlayer = true;
+    private List<Integer> AvailableSquares = new ArrayList<>();
+    private List<Integer> PlayerOneSquares = new ArrayList<>();;
+    private List<Integer> PlayerTwoSquares = new ArrayList<>();;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        for (int i = 0; i < 9; i ++){
+//            Log.i("Instantiating", "onCreate: " + (i + 1));
+//            AvailableSquares.add(i + 1);
+//        }
+        Log.i("SIZE OF AvailabeSquares", "onCreate: " + AvailableSquares.size());
         getViewPositions();
         onScreenTouch(findViewById(R.id.gameBoard));
     }
@@ -23,9 +33,14 @@ public class MainActivity extends AppCompatActivity {
         float x;
         float y;
         String[] squareNames = new String [] {"One", "Two", "Three", "Four", "Five", "Six","Seven", "Eight", "Nine"};
+        int availableSquare = 0;
         for (String squareName : squareNames) {
               final View view = getViewByResourceName(squareName);
               getViewPosition(view);
+              AvailableSquares.add(availableSquare);
+              availableSquare ++;
+            Log.i("AvailableSquares", "Items: " + AvailableSquares.size() + "addingSquare No: " + availableSquare);
+
               outputArrayValues();
         }
     }
@@ -37,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                         view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                         int[] locations = new int[2];
                         view.getLocationOnScreen(locations);
-                        allViewCoords.add(new float [] {locations[0], locations[1] - view.getWidth()/2});
+                        AllViewCoords.add(new float [] {locations[0], locations[1] - view.getWidth()/2});
                         Log.i("getViewPositions", "currentSquareCoord: " + locations[0] +";"+ locations[1]);
                     }       
                 }
@@ -60,9 +75,8 @@ public class MainActivity extends AppCompatActivity {
                     public boolean onTouch(View v, MotionEvent ev) {
                         Log.i("onTouchListener", "onTouch: " + " A touch has happened at: " + ev.getX() + ", " + ev.getY());
                         View dart = findViewById(R.id.red);
-                        touchCoord[0] = ev.getX() - dart.getWidth()/2;
-                        touchCoord[1] = ev.getY() - dart.getHeight()/2;
-                        // float[] fakeDest = new float[] {0.0f, 210.0f};
+                        TouchCoord[0] = ev.getX() - dart.getWidth()/2;
+                        TouchCoord[1] = ev.getY() - dart.getHeight()/2;
                         float[] Destination = determineClosestCoordinates();
                         moveObjectToCoordinates(dart, Destination);
                         return true;
@@ -81,20 +95,39 @@ public class MainActivity extends AppCompatActivity {
         float [] closestCoordinates = new float[]{};
         double smallestDistance = Double.MAX_VALUE;
         double currentDistance;
-        for (float[] i : allViewCoords)
+        int squarePosition = 0;
+        for (float[] i : AllViewCoords)
         {
-            currentDistance = measureDistance(touchCoord,i);
+            currentDistance = measureDistance(TouchCoord,i);
             if (currentDistance < smallestDistance){
                 smallestDistance = currentDistance;
                 closestCoordinates = new float[] {i[0], i[1]};
+                squarePosition = AllViewCoords.indexOf(i);
+
                 Log.i("SelectedCoordinate", "NewCoordinates: " + closestCoordinates[0] + ", " + closestCoordinates[1]);
             }
+        }
+        // find the position of the coordinate in the list:
+        if (IsFirstPlayer) {
+            IsFirstPlayer = false;
+            AvailableSquares.remove(squarePosition);
+            PlayerOneSquares.add(squarePosition);
+            Log.i("Play", "Player One Played Square: " + squarePosition );
+            Log.i("Play", "Available Squares: " + AvailableSquares.size() );
+
+        }
+        else {
+            IsFirstPlayer = true;
+            AvailableSquares.remove(squarePosition);
+            PlayerTwoSquares.add(squarePosition);
+            Log.i("Play", "Player Two Played Square: " + squarePosition );
+            Log.i("Play", "Available Squares: " + AvailableSquares.size() );
         }
         return closestCoordinates;
     }
 
         protected void outputArrayValues (){
-            for (float [] i: allViewCoords
+            for (float [] i: AllViewCoords
                  ) {
                 Log.i("ALLVIEWCOORDS", "index: " + "x: " + i[0] + " y: " + i[1]);
 
@@ -107,6 +140,10 @@ public class MainActivity extends AppCompatActivity {
             float moveToY = Destination[1];
             object.animate().translationX(moveToX).setDuration(1000).start();
             object.animate().translationY(moveToY).setDuration(1000).start();
+        }
+
+        protected void play() {
+
         }
     }
 
